ebd57ad842f28e711c3691853aaa2e31354ebe5f
jochuu <reyn@live.co.uk>
2021-09-20 20:35:05 +0100

    Replace split type radio buttons with automatic split type finder

--- js/main.js
+++ js/main.js
@@ -1,33 +1,38 @@
 let resultContainer = document.querySelector(".resultContainer");
-
-function run() {
-  document.querySelector("#addressBtn").onclick = () => {
-    let splitType = document.querySelector(
-      'input[name="splitType"]:checked'
-    ).value;
-    resultContainer.textContent = "";
-    let latLongTextBox = document.querySelector("#latlong").value;
-
-    let latLongArr = latLongTextBox.split("\n");
-    latLongArr.forEach((latlong, index) => {
-      let latlongArr = latlong.split(splitType);
-      let latitude = latlongArr[0].trim();
-      let longitude = latlongArr[1].trim();
-      $.getJSON(
-        "https://nominatim.openstreetmap.org/reverse",
-        {
-          lat: latitude,
-          lon: longitude,
-          format: "json",
-        },
-        function (result) {
-          console.log(result);
-          let address = document.createElement("div");
-          address.textContent = `[${latitude}${splitType} ${longitude}] ${result.display_name}`;
-          resultContainer.appendChild(address);
-        }
-      );
-    });
-  };
+function determineSplitType(textbox) {
+  if (/[,]/.test(textbox)) {
+    return ",";
+  }
+  if (/[ ]/.test(textbox)) {
+    return " ";
+  }
+  if (/[\t]/.test(textbox)) {
+    return "\t";
+  }
 }
-run();
+document.querySelector("#addressBtn").onclick = () => {
+  let latLongTextBox = document.querySelector("#latlong").value;
+  let splitType = determineSplitType(latLongTextBox);
+  resultContainer.textContent = "";
+
+  let latLongArr = latLongTextBox.split("\n");
+  latLongArr.forEach((latlong, index) => {
+    let latlongArr = latlong.split(splitType);
+    let latitude = latlongArr[0].trim();
+    let longitude = latlongArr[1].trim();
+    $.getJSON(
+      "https://nominatim.openstreetmap.org/reverse",
+      {
+        lat: latitude,
+        lon: longitude,
+        format: "json",
+      },
+      function (result) {
+        console.log(result);
+        let address = document.createElement("div");
+        address.textContent = `[${latitude}${splitType} ${longitude}] ${result.display_name}`;
+        resultContainer.appendChild(address);
+      }
+    );
+  });
+};
